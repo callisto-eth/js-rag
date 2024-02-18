e55bd125780c1e63362776c578717b2dbc62de93
0xKits <kits@callistolabs.xyz>
2024-02-19 00:04:27 +0530

    configured dotenv and gemini

--- app/llm/gemini.ts
+++ app/llm/gemini.ts
@@ -1,1 +1,9 @@
-import {Chat}
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const GeminiModel = new ChatGoogleGenerativeAI({
+	modelName: "gemini-pro",
+	apiKey: process.env.GOOGLE_API_KEY,
+});
